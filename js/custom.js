2a0780591a3985dea9e2f4d1d8caaf0ccfe69fad
Cugry <knownerx@gmail.com>
2022-07-16 12:54:18 +0900

    [WIP]poem

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+function copyUrl() {
+    const element = document.createElement('input');
+    element.value = location.href;
+    document.body.appendChild(element);
+    element.select();
+    document.execCommand('copy');
+    document.body.removeChild(element);
+}
